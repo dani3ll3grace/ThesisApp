36191ca4f7a3263260acfbe628393b8882c56211
dani3113grace <202180186@palawan.edu.ph>
2024-03-04 16:48:26 +0800

    after video 6, fixing sidebar problems

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = "blog"
+# blog:post_list
+# blog:post_detail
+
+
+urlpatterns = [
+
+    path('', views.post_list, name="post_list"),
+    path('<int:id>/', views.post_detail, name="post_detail"),
+]
