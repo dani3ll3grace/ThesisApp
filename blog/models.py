366e952cda76e8e491b4ec88ef7935c98dcbec79
dani3113grace <202180186@palawan.edu.ph>
2024-03-04 17:57:50 +0800

    make canonical and SEO friendly URLs for the blog

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -14,7 +15,10 @@ class Post(models.Model):
 
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    #/blog/2024/1/1/what-am-i
+    
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish')
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                related_name="blog_posts")
@@ -33,6 +37,13 @@ class Post(models.Model):
         indexes = [
             models.Index(fields=['-publish'])
         ]
+
+    def get_absolute_url(self):
+        return reverse("blog:post_detail",
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug],)   
 
     def __str__(self):
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -5,15 +5,21 @@ from django.http import Http404
 
 def post_list(request):
     posts = Post.published.all()
+    # __import__('pdb').set_trace()
     return render(request,
                   "blog/post/list.html",
                   {"posts": posts})
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
      #post = Post.published.get(id=id)
     post = get_object_or_404(Post,
-                             id=id,
-                             status=Post.Status.PUBLISHED)
+                             #id=id,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    
     return render(request,
                    "blog/post/detail.html",
                    {"post":post})
